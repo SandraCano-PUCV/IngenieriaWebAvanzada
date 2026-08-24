b38ea23a77e1ee17aa7e14e18c746d229ac63a34
SandraCano-PUCV <sandra.cano@pucv.cl>
2026-08-23 20:49:07 -0400

    actualización documentaciòn

--- frontend/src/app/components/productos/productos.spec.ts
+++ frontend/src/app/components/productos/productos.spec.ts
@@ -1,21 +1,45 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { of } from 'rxjs';
+import { vi } from 'vitest';
 import { Productos } from './productos';
+import {Products} from '../../services/products';
 
 describe('Productos', () => {
   let component: Productos;
   let fixture: ComponentFixture<Productos>;
 
+  const productosMock = [
+    {
+      id: 1,
+      nombre: 'Notebook',
+      descripcion: 'Notebook de prueba',
+      precio: 500000,
+      stock: 5
+    }
+  ];
+
+  const productosServiceMock = {
+    obtenerProductos: vi.fn().mockReturnValue(
+      of(productosMock)
+    )
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [Productos],
+      providers: [
+        {
+          provide: Products,
+          useValue: productosServiceMock
+        }
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(Productos);
     component = fixture.componentInstance;
     await fixture.whenStable();
   });
-
+ 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
